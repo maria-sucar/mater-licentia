afb5cababc6e71ed66c68558f24cef2f0ef6396c
danilo.andrade333@gmail.com <danilo.andrade333@gmail.com>
2023-07-02 10:39:06 -0300

    language toggle added

--- app.js
+++ app.js
@@ -32,8 +32,21 @@ let penitenciasLista = [
   'Pegue 10 minutos de sol.'
 ]
 
+let penitenciasListaEng = [
+  'Drink 2 liters of water.',
+  'Get 8 hours of sleep tonight.',
+  'Have a good meal.',
+  'Take a relaxing shower.',
+  'Get 10 minutes of sunlight.'
+  ]
+
 function randomPenitencia() {
-  return penitenciasLista[(Math.floor(Math.random() * penitenciasLista.length))];
+  if(siteLangInPortuguese) {
+    return penitenciasLista[(Math.floor(Math.random() * penitenciasLista.length))];
+
+  }else{
+    return penitenciasListaEng[(Math.floor(Math.random() * penitenciasLista.length))];
+  }
 }
 
 sendMessageButton.addEventListener('click', () => {
@@ -44,4 +57,39 @@ sendMessageButton.addEventListener('click', () => {
   penitencia.textContent = penitenciaEscolhida;
 
 
+})
+
+
+
+
+
+/*LANGUAGE TOGGLE*/
+
+const portugueseBtn = document.querySelector(".pt-btn");
+const englishBtn = document.querySelector(".eng-btn");
+const ptbrElements = document.getElementsByClassName('ptbr');
+const engElements = document.getElementsByClassName('eng');
+
+let siteLangInPortuguese = true;
+
+portugueseBtn.addEventListener('click', () => {
+  for (var i = 0; i < ptbrElements.length; i++) {
+    ptbrElements[i].style.display = 'block';
+  }
+  for (var j = 0; j < engElements.length; j++) {
+    engElements[j].style.display = 'none';
+  }
+
+  siteLangInPortuguese = true;
+})
+
+englishBtn.addEventListener('click', () => {
+  for (var i = 0; i < ptbrElements.length; i++) {
+    ptbrElements[i].style.display = 'none';
+  }
+  for (var j = 0; j < engElements.length; j++) {
+    engElements[j].style.display = 'block';
+  }
+
+  siteLangInPortuguese = false;
 })
